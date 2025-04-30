07976f0a30a79cbd20cb660ae68a2445166655c3
Elaine5 <lanne9873@gmail.com>
2025-04-30 15:48:52 -0300

    att

--- conferece-demo/src/main/java/com/pluralsigth/conferece_demo/models/Session.java
+++ conferece-demo/src/main/java/com/pluralsigth/conferece_demo/models/Session.java
@@ -1,7 +1,22 @@
 package com.pluralsigth.conferece_demo.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "sessions")
 public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long session_id;
+    private String session_name;
+    private String session_description;
+    private Integer session_length;
+
+    public Session(){
+
+    }
+
 }
+
